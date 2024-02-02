f7601606006e0794b2d3d5f43c2e316dd61d0289
Ariy83 <143614766+Ariy83@users.noreply.github.com>
2024-02-02 16:59:35 +0200

    done

---
+++ index.js
@@ -0,0 +1,37 @@
+import * as contactService from "./db/contacts.js";
+import { program } from "commander";
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse();
+
+const options = program.opts();
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const allContacts = await contactService.listContacts();
+      return console.table(allContacts);
+
+    case "get":
+      const oneContact = await contactService.getContactById(id);
+      return console.log(oneContact);
+
+    case "add":
+      const newContact = await contactService.addContact(name, email, phone);
+      return console.log(newContact);
+
+    case "remove":
+      const delContact = await contactService.removeContact(id);
+      return console.log(delContact);
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(options);
